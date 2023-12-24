2c77ca4497c423735e0d61514ff11695b98d8dbd
sevgicann <sevgicann.kilic@gmail.com>
2023-12-24 13:47:35 +0300

    commit

---
+++ api_metod.py
@@ -0,0 +1,115 @@
+import pandas as pd
+
+def kanal_bilgi(youtube,kanal_ids):
+    all_data=[]
+
+    #channel: tek bir youtube kanalı hakkında bilgileri içerir
+    #list: Sıfır veya daha fazla kaynak içeren bir liste alır (GET).
+    #API Response kısmından list içinde neleri döndürmek istiyorsak onları alacağız
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id=",".join(kanal_ids)
+    )
+
+    response=request.execute()
+
+    for item in response["items"]:
+        data = {'channelName': item['snippet']['title'],
+                'published':item['snippet']['publishedAt'],
+                'subscribers': item['statistics']['subscriberCount'],
+                'views': item['statistics']['viewCount'],
+                'totalVideos': item['statistics']['videoCount'],
+                'playlistId': item['contentDetails']['relatedPlaylists']['uploads']
+        }
+        all_data.append(data)
+    
+    return pd.DataFrame(all_data)
+
+def collect_video_ids(youtube, playlist_id):
+    """
+    Get list of video IDs of all videos in the given playlist
+    
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    playlist_id: playlist ID of the channel
+    
+    Returns:
+    List of video IDs of all videos in the playlist
+    
+    """
+
+    video_ids = []
+
+    request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            playlistId=playlist_id,
+            maxResults=50
+        )
+    response = request.execute()
+
+    for item in response['items']:
+        video_ids.append(item['contentDetails']['videoId'])
+
+    next_page_token = response.get('nextPageToken')
+    
+    while next_page_token is not None:
+        request = youtube.playlistItems().list(
+                part="snippet,contentDetails",
+                playlistId=playlist_id,
+                maxResults=50,
+                pageToken=next_page_token
+            )
+        response = request.execute()
+
+        for item in response['items']:
+            video_ids.append(item['contentDetails']['videoId'])
+
+        next_page_token = response.get('nextPageToken')    
+
+        
+    return video_ids
+
+def collect_video_detay(youtube, video_ids):
+    """
+    Get video statistics of all videos with given IDs
+    
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    video_ids: list of video IDs
+    
+    Returns:
+    Dataframe with statistics of videos, i.e.:
+        'channelTitle', 'title', 'description', 'tags', 'publishedAt'
+        'viewCount', 'likeCount', 'favoriteCount', 'commentCount'
+        'duration', 'definition', 'caption'
+    """
+        
+    all_video_info = []
+    
+    for i in range(0, len(video_ids), 50):
+        request = youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=','.join(video_ids[i:i+50])
+        )
+        response = request.execute() 
+
+        for video in response['items']:
+            stats_to_keep = {'snippet': ['channelTitle', 'title', 'description', 'tags', 'publishedAt'],
+                             'statistics': ['viewCount', 'likeCount', 'favouriteCount', 'commentCount'],
+                             'contentDetails': ['duration', 'definition', 'caption']
+                            }
+            video_info = {}
+            video_info['video_id'] = video['id']
+
+            for k in stats_to_keep.keys():
+                for v in stats_to_keep[k]:
+                    try:
+                        video_info[v] = video[k][v]
+                    except:
+                        video_info[v] = None
+
+            all_video_info.append(video_info)
+            
+    return pd.DataFrame(all_video_info)
